c0cd80da43c8da2f4a164952a0b041555b1cc631
Cassian Godsted <cassian@godsted.com>
2025-04-16 18:42:55 -0400

    hotfix oldforge

--- forge/src/main/java/cc/cassian/clickthrough/helpers/forge/ModHelpersImpl.java
+++ forge/src/main/java/cc/cassian/clickthrough/helpers/forge/ModHelpersImpl.java
@@ -1,6 +1,13 @@
 package cc.cassian.clickthrough.helpers.forge;
 
 
+import net.minecraft.block.BlockState;
+//? if >1.20 {
+import net.minecraft.registry.tag.BlockTags;
+//?} else {
+/*import net.minecraft.tag.BlockTags;
+ *///?}
+import net.minecraftforge.common.Tags;
 import net.minecraftforge.fml.ModList;
 
 public class ModHelpersImpl {
@@ -10,6 +17,13 @@ public class ModHelpersImpl {
 
     public static boolean isLoaded(String mod) {
         return ModList.get().isLoaded(mod);
+    }
+
+    public static boolean isTaggedAsContainer(BlockState state) {
+        var stack = state.getBlock().asItem().getDefaultStack();
+        return state.isIn(Tags.Blocks.CHESTS) || state.isIn(BlockTags.GUARDED_BY_PIGLINS)
+                || stack.isIn(Tags.Items.CHESTS)
+                || state.isIn(Tags.Blocks.BARRELS)  || stack.isIn(Tags.Items.BARRELS);
     }
 
 }
